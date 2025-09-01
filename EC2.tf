2a185610450b7fd971f02351bf425c10ded6b829
Imran <mohammadabdulimran93@gmail.com>
2025-09-01 11:35:45 -0500

    changed cidr_block to cidr_blocks in ec2 file

--- EC2.tf
+++ EC2.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "awsec2demo" {
         from_port   = 8080
         to_port     = 8080
         protocol    = "tcp"
-        cidr_block  = ["0.0.0.0/0"]
+        cidr_blocks  = ["0.0.0.0/0"]
     }
 }
 
